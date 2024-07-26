6429091775895f02f54df3159b9644f9a7c6fa77
trivu9001 <trivu9001@gmail.com>
2024-07-27 05:14:28 +0700

    Update Rank.jsx

--- src/Pages/Rank/Rank.jsx
+++ src/Pages/Rank/Rank.jsx
@@ -48,7 +48,7 @@ const Rank = () => {
                   <th>Hạng</th>
                   <th>Họ Tên</th>
                   <th>Tổng thời gian làm bài tính theo phút</th>
-                  <th>Tổng số câu đã làm</th>
+                  <th>Tổng số bài học</th>
                   <th></th>
                 </tr>
               </thead>
